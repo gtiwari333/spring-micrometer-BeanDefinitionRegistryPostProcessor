3489bd2f3c2fdaad1f6ffa6351bc78ad3d798e89
Ganesh T <gtiwari333@gmail.com>
2024-02-17 19:18:35 -0600

    works

--- src/main/java/beanns/BeansApp.java
+++ src/main/java/beanns/BeansApp.java
@@ -1,36 +1,29 @@
 package beanns;
 
-import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.tracing.Tracer;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.observation.ObservationAutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -50,27 +43,15 @@ public class BeansApp {
     }
 }
 
-@AutoConfigureAfter(value = {ObservationAutoConfiguration.class, RestTemplateAutoConfiguration.class, JacksonAutoConfiguration.class
-        , GsonAutoConfiguration.class})
-//@AutoConfigureBefore()
-class Con {
+@Configuration
+class Config {
 
     @Bean
-//    static BeanRegistry reg(RestTemplate t) { //Issue 1: the trace ids doesn't print on log
-    static DynamicBeanDefinitionRegistrar reg(Environment env, ObservationRegistry tr, Tracer tt) { //Issue 2: the trace ids doesn't print on log
-//    static BeanRegistry reg() { // NON ISSUE: trace ids print on log
-        return new DynamicBeanDefinitionRegistrar(env);//TODO: pass RestTemplate and Tracer to the dynamically created bean
+    static BeanRegistry reg(ApplicationContext ctx) {
+        return new BeanRegistry(ctx);
     }
 }
 
-
-//@RequiredArgsConstructor
-//class BeanRegistry implements BeanDefinitionRegistryPostProcessor {
-//
-//    @Override
-//    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry reg) throws BeansException {
-//    }
-//}
 
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 @Data
@@ -94,25 +75,30 @@ class AService {
     final RestTemplate rest;
     final App app;
     final ABean aBean;
+    final BBean bBean;
 
-    @Scheduled(fixedDelay = 1000)
+
+    @Scheduled(fixedDelay = 4000)
     void x() {
         log.info(app.name);
-        ResponseEntity<String> resp = rest.getForEntity("http://localhost:8080/test", String.class);
+        ResponseEntity<String> resp = rest.getForEntity("http://localhost:8080/test/scheduled-job", String.class);
         log.info(resp.getStatusCode() + " " + resp.getBody());
         aBean.doSth();
+        bBean.doSth();
+        log.info("");
     }
 }
 
 @RequiredArgsConstructor
-class DynamicBeanDefinitionRegistrar implements BeanDefinitionRegistryPostProcessor {
+@Slf4j
+class BeanRegistry implements BeanDefinitionRegistryPostProcessor {
 
-    private final Environment env;
+    private final ApplicationContext ctx;
 
     @Override
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry)
             throws BeansException {
-        BindResult<App> result = Binder.get(env)
+        BindResult<App> result = Binder.get(ctx.getEnvironment())
                 .bind("app", App.class);
         App app = result.get();
 
@@ -129,7 +115,13 @@ class DynamicBeanDefinitionRegistrar implements BeanDefinitionRegistryPostProces
                     beanDefinition.setBeanClass(c);
                     beanDefinition.setInstanceSupplier(() -> {
                         try {
-                            return c.newInstance();
+                            RestTemplate restTemplate = ctx.getBean(RestTemplate.class);
+
+                            Object obj = c.newInstance();
+                            c.getMethod("setRestTemplate", RestTemplate.class).invoke(obj, restTemplate);
+
+
+                            return obj;
                         } catch (Exception e) {
                             throw new RuntimeException(e);
                         }
@@ -144,38 +136,53 @@ class DynamicBeanDefinitionRegistrar implements BeanDefinitionRegistryPostProces
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 class Ctrl {
 
-    @GetMapping("/test")
-    String test() {
-        log.info("Test ..");
+    final RestTemplate restTemplate;
+
+    @GetMapping("/test/{source}")
+    String test(@PathVariable String source) {
+        log.info("Test called from {} ", source);
         return "Hello";
     }
 
 }
 
 @Slf4j
+@Setter
 class ABean {
+
+    RestTemplate restTemplate;
 
     void doSth() {
         log.info("Called A Bean method");
+        ResponseEntity<String> resp = restTemplate.getForEntity("http://localhost:8080/test/A-Bean", String.class);
+        log.info(resp.getStatusCode() + " " + resp.getBody());
     }
 
 }
 
 @Slf4j
+@Setter
 class BBean {
 
-    void doSth() {
-        log.info("Called");
+    RestTemplate restTemplate;
 
+    void doSth() {
+        log.info("Called B Bean method");
+        ResponseEntity<String> resp = restTemplate.getForEntity("http://localhost:8080/test/C-Bean", String.class);
+        log.info(resp.getStatusCode() + " " + resp.getBody());
     }
 
 }
 
 @Slf4j
+@Setter
 class CBean {
 
+    RestTemplate restTemplate;
+
     void doSth() {
         log.info("Called");
 
@@ -184,7 +191,10 @@ class CBean {
 }
 
 @Slf4j
+@Setter
 class DBean {
+
+    RestTemplate restTemplate;
 
     void doSth() {
         log.info("Called");
